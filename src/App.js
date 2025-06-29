7770b65d17b581186ec93d7b95acb3f99c11fbe8
Raja <kraj9380286@gmail.com>
2025-06-29 22:57:10 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function onfirstHandler(event){
+    console.log(event.target.value);
+  }
+  function onLastHandler(event){
+    console.log(event.target.value);
+  }
   return (
     <div className="App">
       <form>
-   <input type='text' placeholder='first Name'/>
+   <input type='text' placeholder='first Name' onChange={onfirstHandler}/>
    <br/>
    <br/>
-   <input type='text' placeholder='Last Name'/>
+   <input type='text' placeholder='Last Name'  onChange={onLastHandler}/>
    </form>
     </div>
   );
